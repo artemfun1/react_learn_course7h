5d99cae44ddfa4f63c40b4875709830ec9212985
Artem Peshkov <a.o.peshkov@yandex.ru>
2023-12-29 17:19:12 +0500

    cool learning

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import CostItem from "./components/CostItem";
+import Costs from './components/Costs/Costs'
+import NewCost from './components/NewCost/NewCost'
 
+  //TODO: some
+  // FIXME: some
+
+//"#449eff"  "#51abff"  "#4ba2ff"
 function App() {
+	const costs = [
+		{
+			date: new Date(2021, 2, 12),
+			description: 'Холодильник',
+			amount: 999.99,
+		}, 
+		{
+			date: new Date(2021, 11, 25),
+			description: 'MackBook',
+			amount: 1254.72,
+		},
+		{
+			date: new Date(2021, 3, 12),
+			description: 'Джинсы',
+			amount: 49.99,
+		},
+	];
 
-  const costs =[
-    {
-      date:new Date(2021,2,12),
-      description: 'Холодильник',
-      amount: 999.99  
-    },
-    {
-      date:new Date(2021,11,25),
-      description: 'MackBook',
-      amount: 1254.72  
-    },
-    {
-      date:new Date(2021,3,12),
-      description: 'Джинсы',
-      amount: 49.99 
-    }
-  ]
-
-  return (
-    <div>
-      <h1>Hello</h1>
-      <CostItem 
-      date={costs[0].date}
-      description={costs[0].description}
-      amount={costs[0].amount}
-        />
-      <CostItem
-      date={costs[1].date}
-      description={costs[1].description}
-      amount={costs[1].amount}/>
-      <CostItem
-      date={costs[2].date}
-      description={costs[2].description}
-      amount={costs[2].amount}/>
-    </div>
-  );
+	return (
+		<div>
+			<NewCost/>
+			<Costs costs={costs} />
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// import ReactDOM from 'react-dom'
+// ReactDOM.render(<App/> , document.getElementById('root'));
 
-ReactDOM.render(<App/> , document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(
+root.render(
   // <React.StrictMode>
-    // <App />
+    <App />
   // </React.StrictMode>
-// );
+);
